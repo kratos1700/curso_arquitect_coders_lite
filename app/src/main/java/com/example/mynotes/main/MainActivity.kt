7eac80095f10d4ffb5e76375071ed3e2470bfd49
Kratos1700 <hunterstarsa@hotmail.com>
2023-03-26 19:30:34 +0200

    añadido en el viewmodel opcion del state con LiveData

--- app/src/main/java/com/example/mynotes/main/MainActivity.kt
+++ app/src/main/java/com/example/mynotes/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mynotes.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.example.mynotes.NotesApplication
 import com.example.mynotes.NotesDatabase
@@ -9,32 +10,40 @@ import com.example.mynotes.databinding.ActivityMainBinding
 import com.example.mynotes.detail.DetailActivity
 import kotlinx.coroutines.launch
 
-lateinit var binding: ActivityMainBinding
-lateinit var notesAdapter: NotesAdapter
-lateinit var database: NotesDatabase
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var notesAdapter: NotesAdapter
+
+
+    //creamos una factori para recuperar los datos que le pasamos por parametro
+    private val vm by viewModels<MainViewModel>{
+        MainViewModelFactory(
+            (application as NotesApplication).notesDatabase
+        )
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        database = (application as NotesApplication).notesDatabase
         binding = ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(root)
             notesAdapter = NotesAdapter(
                 onNoteClick = { DetailActivity.navigate(this@MainActivity, it.id) },
                 onNoteDelete = {
-                    lifecycleScope.launch {
-                        database.notesDao().delete(it)
-                        notesAdapter.submitList(database.notesDao().getAll())
-                    }
-
-                }
+                    vm.onNoteDelete(it) }
 
             )
             notesList.adapter = notesAdapter
 
             fab.setOnClickListener {
-
                 DetailActivity.navigate(this@MainActivity)
+            }
+            //observamos los cambios del estado
+            vm.state.observe(this@MainActivity){
+                notesAdapter.submitList(it)
             }
         }
 
@@ -42,10 +51,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        lifecycleScope.launch {
-            val notes = database.notesDao().getAll()
-            notesAdapter.submitList(notes)
-        }
+        vm.onResume()
+
     }
 
 }
--- app/src/main/java/com/example/mynotes/main/MainViewModel.kt
+++ app/src/main/java/com/example/mynotes/main/MainViewModel.kt
@@ -1,8 +1,43 @@
 package com.example.mynotes.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.mynotes.Note
+import com.example.mynotes.NotesDatabase
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
+// pasamos por parametro el NotesDatabase
+class MainViewModel(private val db:NotesDatabase) : ViewModel() {
 
+    //necesitamos un estado que represente el estado del UI,
+    // si se cambia lo representa
+    private val _state = MutableLiveData<List<Note>>()
+    val state : MutableLiveData<List<Note>> = _state
+
+
+    fun onResume() {
+        viewModelScope.launch {
+           _state.value = db.notesDao().getAll()
+        }
+    }
+
+    fun onNoteDelete(nota: Note) {
+        viewModelScope.launch {
+        //eliminamos nota
+            db.notesDao().delete(nota)
+            //actualizamos el estado con las notas actuales
+            _state.value= db.notesDao().getAll()
+        }
+    }
+
+}
+
+@Suppress("UNCHECKED_CAST")
+class MainViewModelFactory(private val db:NotesDatabase): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(db) as T
+    }
 
 }
